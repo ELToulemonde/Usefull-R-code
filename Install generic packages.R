ce24ebadf121625f6a59e485ba73df4024981906
ELToulemonde <el.toulemonde@protonmail.com>
2017-10-26 10:55:11 +0200

    Add files via upload

---
+++ Install generic packages.R
@@ -0,0 +1,44 @@
+## Set repo
+options(repos='http://cran.rstudio.com/')
+
+## Data formats
+install.packages("data.table")
+install.packages("matrixcalc")
+install.packages("lubridate")
+install.packages("rsdmx")
+
+install.packages("reshape")
+install.packages("reshape2")
+install.packages("stringr")
+
+install.packages("dataPreparation")
+
+## Machine learning models
+install.packages("randomForest")
+install.packages("nnet")
+install.packages("kohonen")
+install.packages("ROCR")
+
+## Visualization packages
+install.packages("ggplot2")
+
+## Parallel computation
+install.packages("foreach")
+install.packages("doSNOW")
+install.packages("snowfall")
+
+## Code packaging
+install.packages("devtools")
+install.packages("testthat")
+install.packages("RUnit")
+install.packages("roxygen2")
+install.packages("knitr")
+install.packages("rmarkdown")
+install.packages("kableExtra")
+install.packages("pander")
+install.packages("lintr")
+
+## Others
+install.packages("httr")
+install.packages("bit64")
+install.packages("microbenchmark")
